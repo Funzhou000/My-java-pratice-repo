a03d8adef3d5feda71cfabd3a760eeeac7974c95
Funzhou <li15217962247@outlook.com>
2025-11-07 21:22:01 +0800

    D8  bit of pratice about Class 我要换mac啦

---
+++ D8/GrialFriends.java
@@ -0,0 +1,17 @@
+package D8;
+
+public class GrialFriends {
+    // 属性初始化
+    int age;
+    String name;
+    String gender;
+
+    // 成员方法初始化
+    public void sleep() {
+        System.out.println("good night Funzhou");
+    }
+
+    public void meal() {
+        System.out.println("Do u want to try DairyQueen?");
+    }
+}
---
+++ D8/GrialFriendsTest.java
@@ -0,0 +1,11 @@
+package D8;
+
+public class GrialFriendsTest {
+    public static void main(String[] args) {
+        GrialFriends myGF = new GrialFriends();
+        myGF.age = 18;
+        myGF.gender = "grial";
+        myGF.sleep();
+        myGF.meal();
+    }
+}
